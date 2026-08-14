9810a8143051b6c9f550430bae8a6c452601aeaf
wickidcow <jaywago@gmail.com>
2026-08-13 20:03:20 -0400

    Replace deprecated woodcutter android effects

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/WoodcutterAndroid.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/WoodcutterAndroid.java
@@ -10,6 +10,7 @@ import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.utils.VisualEffectUtils;
 import io.github.thebusybiscuit.slimefun4.utils.tags.SlimefunTag;
 import java.util.List;
 import java.util.UUID;
@@ -17,7 +18,6 @@ import java.util.function.Predicate;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.bukkit.Bukkit;
-import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Tag;
@@ -53,7 +53,7 @@ public class WoodcutterAndroid extends ProgrammableAndroid {
 
             if (!list.isEmpty()) {
                 Block log = list.get(list.size() - 1);
-                log.getWorld().playEffect(log.getLocation(), Effect.STEP_SOUND, log.getType());
+                VisualEffectUtils.playBlockBreakEffect(log);
 
                 OfflinePlayer owner = Bukkit.getOfflinePlayer(UUID.fromString(
                         StorageCacheUtils.getUniversalBlockData(menu.getUuid(), b.getLocation(), "owner")));
@@ -79,7 +79,7 @@ public class WoodcutterAndroid extends ProgrammableAndroid {
             log.getWorld().dropItemNaturally(log.getLocation(), remainder);
         }
 
-        log.getWorld().playEffect(log.getLocation(), Effect.STEP_SOUND, log.getType());
+        VisualEffectUtils.playBlockBreakEffect(log);
 
         // If the android just chopped the bottom log, we replant the appropriate sapling
         if (log.getY() == android.getRelative(face).getY()) {
