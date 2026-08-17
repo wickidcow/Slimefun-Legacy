14c4a5b9a0895492af6054364b44ebcc6d8770b7
wickidcow <jaywago@gmail.com>
2026-08-16 22:44:41 -0400

    fix: commit auto disenchanter inputs at completion

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/enchanting/AutoDisenchanter.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/enchanting/AutoDisenchanter.java
@@ -7,6 +7,7 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.items.virtual.VirtualItemHandler.InventoryContext;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.implementation.operations.CraftingOperation;
 import io.github.thebusybiscuit.slimefun4.integrations.AdvancedEnchantmentsIntegration;
 import java.util.Collections;
 import java.util.HashMap;
@@ -230,6 +231,22 @@ public class AutoDisenchanter extends AbstractEnchantmentMachine {
             return null;
         }
         return recipe;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    protected boolean commitOperationInputs(BlockMenu menu, CraftingOperation operation) {
+        boolean consumed = EnchantmentMachineRuntime.consumeOneEachIfUnchanged(
+                menu, getInputSlots(), operation.getIngredients());
+        if (!consumed) {
+            EnchantmentMachineRuntime.status(
+                    menu,
+                    Material.BARRIER,
+                    "&eCompletion paused",
+                    "&7The original item and book must still",
+                    "&7be present before outputs can be created.");
+        }
+        return consumed;
     }
 
     @ParametersAreNonnullByDefault
