35e3f227508ff6ffa79bb37dfe5d9203a794fe45
wickidcow <jaywago@gmail.com>
2026-08-27 20:02:40 -0400

    perf: streamline auto-crafter tick hot path

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/autocrafters/AbstractAutoCrafter.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/autocrafters/AbstractAutoCrafter.java
@@ -205,41 +205,37 @@ public abstract class AbstractAutoCrafter extends SlimefunItem implements Energy
      */
     protected void tick(@Nonnull Block b, @Nonnull SlimefunBlockData data) {
         AbstractRecipe recipe = getSelectedRecipe(b);
+        Location location = b.getLocation();
+        int energyConsumption = getEnergyConsumption();
 
-        if (recipe == null || !recipe.isEnabled() || getCharge(b.getLocation(), data) < getEnergyConsumption()) {
+        if (recipe == null || !recipe.isEnabled() || getCharge(location, data) < energyConsumption) {
             // No recipe / disabled recipe / no energy, abort...
             return;
         }
 
         // The block below where we would expect our inventory holder.
         Block targetBlock = b.getRelative(BlockFace.DOWN);
+        boolean hasInteractor = CrafterInteractorManager.hasInterator(targetBlock);
+        CrafterInteractable customInteractor = hasInteractor ? CrafterInteractorManager.getInteractor(targetBlock) : null;
 
         // Check if special interactor used. If so, check the recipe.
-        if (CrafterInteractorManager.hasInterator(targetBlock)) {
+        if (customInteractor != null) {
             // Check if recipe change. If so, update the count...
+            ItemStack recipeResult = recipe.getResult();
             ItemStack cachedRecipeResult = recipeCache.get(b);
 
             if (cachedRecipeResult == null
-                    || !SlimefunUtils.isItemSimilar(recipe.getResult(), cachedRecipeResult, true, false)) {
-                recipeCache.put(b, recipe.getResult());
-                CrafterInteractorManager.getInteractor(targetBlock)
-                        .setIngredientCount(targetBlock, getIngredientCount(recipe));
+                    || !SlimefunUtils.isItemSimilar(recipeResult, cachedRecipeResult, true, false)) {
+                recipeCache.put(b, recipeResult);
+                customInteractor.setIngredientCount(targetBlock, getIngredientCount(recipe));
             }
-        }
-
-        // If recipe noe enabled or no enough charge, return
-        if (!recipe.isEnabled() || getCharge(b.getLocation(), data) < getEnergyConsumption()) {
-            return;
         }
 
         // Make sure this is interactable
         if (isValidInventory(targetBlock)) {
-            CrafterInteractable interactor = null;
+            CrafterInteractable interactor = customInteractor;
 
-            if (CrafterInteractorManager.hasInterator(targetBlock)) {
-                // Has valid interactor
-                interactor = CrafterInteractorManager.getInteractor(targetBlock);
-            } else {
+            if (!hasInteractor) {
                 // No custom interactor, check if the vanilla inventory
                 BlockState state = targetBlock.getState(false);
                 if (state instanceof InventoryHolder) {
@@ -252,9 +248,9 @@ public abstract class AbstractAutoCrafter extends SlimefunItem implements Energy
             if (interactor != null) {
                 if (craft(interactor, recipe)) {
                     // We are done crafting!
-                    Location loc = b.getLocation().add(0.5, 0.8, 0.5);
-                    b.getWorld().spawnParticle(VersionedParticle.HAPPY_VILLAGER, loc, 6);
-                    removeCharge(b.getLocation(), getEnergyConsumption());
+                    Location particleLocation = location.clone().add(0.5, 0.8, 0.5);
+                    b.getWorld().spawnParticle(VersionedParticle.HAPPY_VILLAGER, particleLocation, 6);
+                    removeCharge(location, energyConsumption);
                 }
             }
         } else recipeCache.remove(b);
@@ -388,7 +384,7 @@ public abstract class AbstractAutoCrafter extends SlimefunItem implements Energy
      * This shows the given {@link AbstractRecipe} to the {@link Player} in a preview window.
      *
      * @param p
-     *            The {@link Player}
+     *            The {@link Player} to show them
      * @param b
      *            The {@link Block} of the {@link AbstractAutoCrafter}
      * @param recipe
