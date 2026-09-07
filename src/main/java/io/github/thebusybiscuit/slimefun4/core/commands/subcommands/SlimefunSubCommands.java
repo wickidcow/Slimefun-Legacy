23b06c19943bb56e7f6f7611a0334400a2375cad
wickidcow <jaywago@gmail.com>
2026-09-07 10:59:56 -0400

    Route Doctor migration diagnostics

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/SlimefunSubCommands.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/SlimefunSubCommands.java
@@ -51,7 +51,7 @@ public final class SlimefunSubCommands {
         commands.add(new UnbanItemCommand(plugin, cmd));
         commands.add(new ClearDataCommand(plugin, cmd));
         commands.add(new StabilityCommand(plugin, cmd));
-        commands.add(new DoctorCommand(plugin, cmd));
+        commands.add(new DoctorRouterCommand(plugin, cmd));
         return commands;
     }
 }
