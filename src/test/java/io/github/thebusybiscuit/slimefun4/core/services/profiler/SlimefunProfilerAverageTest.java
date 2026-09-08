b9ebe89d58ccb789ab1605952925948933ea50ad
wickidcow <jaywago@gmail.com>
2026-09-08 18:22:06 -0400

    test: cover explicit start during telemetry

--- src/test/java/io/github/thebusybiscuit/slimefun4/core/services/profiler/SlimefunProfilerAverageTest.java
+++ src/test/java/io/github/thebusybiscuit/slimefun4/core/services/profiler/SlimefunProfilerAverageTest.java
@@ -81,6 +81,27 @@ class SlimefunProfilerAverageTest {
     }
 
     @Test
+    void explicitStartWaitsForActiveTelemetrySample() throws ReflectiveOperationException {
+        SlimefunProfiler profiler = new SlimefunProfiler();
+
+        profiler.startTelemetry();
+        profiler.start();
+
+        assertTrue(profiler.isProfiling());
+        assertTrue(booleanField(profiler, "telemetryProfiling"));
+        assertTrue(booleanField(profiler, "pendingExplicitStart"));
+
+        Method completeProfileCycle = SlimefunProfiler.class.getDeclaredMethod("completeProfileCycle");
+        completeProfileCycle.setAccessible(true);
+        completeProfileCycle.invoke(profiler);
+
+        assertTrue(profiler.isProfiling());
+        assertFalse(booleanField(profiler, "telemetryProfiling"));
+        assertFalse(booleanField(profiler, "pendingExplicitStart"));
+        profiler.kill();
+    }
+
+    @Test
     void explicitStartRemainsUnconditional() {
         SlimefunProfiler profiler = new SlimefunProfiler();
 
@@ -130,6 +151,12 @@ class SlimefunProfilerAverageTest {
                 return SummaryOrderType.HIGHEST;
             }
         };
+    }
+
+    private boolean booleanField(SlimefunProfiler profiler, String name) throws ReflectiveOperationException {
+        Field field = SlimefunProfiler.class.getDeclaredField(name);
+        field.setAccessible(true);
+        return field.getBoolean(profiler);
     }
 
     private AtomicLong atomicLong(SlimefunProfiler profiler, String name) throws ReflectiveOperationException {
