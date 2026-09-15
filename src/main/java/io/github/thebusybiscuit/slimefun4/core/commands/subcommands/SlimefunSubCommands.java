5ff3af7267a60da3c9043d57807323c9e3440936
wickidcow <jaywago@gmail.com>
2026-09-14 22:04:34 -0400

    Keep Doctor invariant with machine wrapper

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/SlimefunSubCommands.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/SlimefunSubCommands.java
@@ -52,7 +52,8 @@ public final class SlimefunSubCommands {
         commands.add(new UnbanItemCommand(plugin, cmd));
         commands.add(new ClearDataCommand(plugin, cmd));
         commands.add(new StabilityCommand(plugin, cmd));
-        commands.add(new DoctorMachineRouterCommand(plugin, cmd));
+        DoctorRouterCommand doctor = new DoctorRouterCommand(plugin, cmd);
+        commands.add(new DoctorMachineRouterCommand(plugin, cmd, doctor));
         return commands;
     }
 }
