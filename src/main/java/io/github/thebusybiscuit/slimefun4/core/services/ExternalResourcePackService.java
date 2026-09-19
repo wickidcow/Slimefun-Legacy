39d37195abc03a0a9a9746f6d681bc017e1e8096
wickidcow <jaywago@gmail.com>
2026-09-19 09:32:19 -0400

    fix(resource-pack): read Legacy settings from configSFLAddons

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/services/ExternalResourcePackService.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/services/ExternalResourcePackService.java
@@ -1,6 +1,7 @@
 package io.github.thebusybiscuit.slimefun4.core.services;
 
 import io.github.thebusybiscuit.slimefun4.core.config.CuriositiesConfig;
+import io.github.thebusybiscuit.slimefun4.core.config.CuriositiesConfig;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import java.net.URI;
 import java.net.URISyntaxException;
